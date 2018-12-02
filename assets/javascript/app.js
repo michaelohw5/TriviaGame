475279e2eba39b987d60a6360f9dbe8c70bb5c65
mohw <michaelohw5@gmail.com>
2018-12-01 23:15:57 -0800

    in progress. stuck.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,18 @@
 $(document).ready(function () {
     //variables
-    var questions = ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7"];
-    var answers = [{ ans1: "a1", ans2: "a2", ans3: "a3", ans4: "a4" } //A1
-        , { ans1: "a5", ans2: "a6", ans3: "a7", ans4: "a8" } //A2
-        , { ans1: "a9", ans2: "a10", ans3: "a11", ans4: "a12" } //A3
-        , { ans1: "a13", ans2: "a14", ans3: "a15", ans4: "a16" } //A4
-        , { ans1: "a17", ans2: "a18", ans3: "a19", ans4: "a20" } //A5
-        , { ans1: "a21", ans2: "a22", ans3: "a23", ans4: "a24" } //A6
-        , { ans1: "a25", ans2: "a26", ans3: "a27", ans4: "a28" }]; //A7
-    var correctAnswers = [1, 2, 3, 4, 1, 2, 3];
+    var questions = [{q:"Q1", ans1: "a1", ans2: "a2", ans3: "a3", ans4: "a4", correct: "a1", used: false} //A1
+    , {q:"Q2",  ans1: "a5", ans2: "a6", ans3: "a7", ans4: "a8", correct: "a6", used: false } //A2
+    , {q:"Q3",  ans1: "a9", ans2: "a10", ans3: "a11", ans4: "a12", correct: "a11", used: false } //A3
+    , {q:"Q4",  ans1: "a13", ans2: "a14", ans3: "a15", ans4: "a16", correct: "a16", used: false } //A4
+    , {q:"Q5",  ans1: "a17", ans2: "a18", ans3: "a19", ans4: "a20", correct: "a18", used: false } //A5
+    , {q:"Q6",  ans1: "a21", ans2: "a22", ans3: "a23", ans4: "a24", correct: "a21", used: false } //A6
+    , {q:"Q7",  ans1: "a25", ans2: "a26", ans3: "a27", ans4: "a28", correct: "a26", used: false }]; //A7
+    // var questions = ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7"];
+
     var questionIndex;
     var currentQ;
     var currentA;
+    var correctA;
     var numCorrect = 0;
     var numWrong = 0;
     var timerElem = $("#timer");
@@ -20,8 +21,12 @@ $(document).ready(function () {
     var ans2 = $("#answer2");
     var ans3 = $("#answer3");
     var ans4 = $("#answer4");
+    var qSection = $("#question");
     var chosenAns = 0;
-    var maxTime = 15;
+    var maxTime = 5;
+    var time = 5;
+    var intervalId;
+    var fiveSecIntervalId;
     //rand number generator
     var randNumGen = function getRandomIntInclusive(min, max) {
         min = Math.ceil(min);
@@ -29,59 +34,139 @@ $(document).ready(function () {
         return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
     } //works cuz copy pasta
 
-    var chooseQuestion = function() {
-        questionIndex = randNumGen(0, 6);
-        currentQ = questions[questionIndex];
-        currentA = answers[questionIndex];
-        ans1.text(currentA.ans1);
-        ans2.text(currentA.ans2);
-        ans3.text(currentA.ans3);
-        ans4.text(currentA.ans4);
+    var run = function () {
+        intervalId = setInterval(decrement, 1000);
     }
 
-    var compareAnswer = function() {
-        if (chosenAns === correctAnswers[questionIndex]) {
-            console.log("yay");
-            numCorrect++;
+    var fiveSecTimer = function () {
+        fiveSecIntervalId = setInterval(nextQ, 5000);
+    }
+
+    var nextQ = function () {
+        clearInterval(fiveSecIntervalId);
+        restartGame();
+        run();
+    }
+
+    var restartGame = function() {
+        chooseQuestion();
+    }
+
+    var decrement = function () {
+        time--;
+        timerElem.text(time);
+        if (time === 0) {
+            stop();
+            //run wrong answer page
+            pickedWrong();
+            //start 5sec timer
+        }
+    }
+
+    var stop = function () {
+        clearInterval(intervalId);
+    }
+
+    var checkIfDone = function() {
+        var questionsDone = 0;
+        for (var i = 0; i<questions.length; i++) {
+            if (questions[i].used === true) {
+                questionsDone++;
+            }
+        }
+        if (questionsDone === questions.length) {
+            return true;
         }
         else {
-            console.log("nay");
-            numWrong++;
+            return false;
+            chooseQuestion();
+        }
+    }
+
+    var chooseQuestion = function () {
+        questionIndex = randNumGen(0, 6);
+        if (answers[questionIndex.used === false]) {
+            answers[questionIndex.used] = true;
+            currentQ = questions[questionIndex];
+            currentA = answers[questionIndex].correct;
+            qSection.text(currentQ);
+            ans1.text(currentA.ans1);
+            ans2.text(currentA.ans2);
+            ans3.text(currentA.ans3);
+            ans4.text(currentA.ans4);
+            time = maxTime;
+    }
+
+    var pickedRight = function () {
+        console.log("yay");
+        numCorrect++;
+        qSection.text("You Are Correct!!");
+        stop();
+        fiveSecTimer();
+    }
+
+    var pickedWrong = function (answer) {
+        console.log("nay");
+        numWrong++;
+        qSection.text("Buzz!! Correct Answer is " + answer);
+        stop();
+        fiveSecTimer();
+    }
+
+    var compareAnswer = function () {
+        correctA = answers[questionIndex].correct;
+        if (chosenAns === answers[questionIndex].correct) {
+            pickedRight();
+        }
+        else {
+            pickedWrong(correctA);
         }
     }
 
     //button functions
     startBtn.click(function () {
         initGame();
+        stop();
+        run();//timer
     }); //start button
 
     ans1.click(function () {
-        chosenAns = 1;
+        chosenAns = ans1.text();
         compareAnswer();
+        console.log(chosenAns);
     }); //answer #1
 
     ans2.click(function () {
-        chosenAns = 2;
+        chosenAns = ans2.text();
         compareAnswer();
     }); //answer #2
 
     ans3.click(function () {
-        chosenAns = 3;
+        chosenAns = ans3.text();
         compareAnswer();
     }); //answer #3
 
     ans4.click(function () {
-        chosenAns = 4;
+        chosenAns = ans4.text();
         compareAnswer();
     }); //answer #4
 
     var initGame = function () {
-        timerElem.text("15");
+        timerElem.text("5");
         startBtn.text("Restart");
         chooseQuestion();
         console.log(currentQ);
         console.log(currentA);
     }
 
-
 });
+
+
+
+    // var answers = [{ ans1: "a1", ans2: "a2", ans3: "a3", ans4: "a4", correct: "a1", used: false} //A1
+    //     , { ans1: "a5", ans2: "a6", ans3: "a7", ans4: "a8", correct: "a6", used: false } //A2
+    //     , { ans1: "a9", ans2: "a10", ans3: "a11", ans4: "a12", correct: "a11", used: false } //A3
+    //     , { ans1: "a13", ans2: "a14", ans3: "a15", ans4: "a16", correct: "a16", used: false } //A4
+    //     , { ans1: "a17", ans2: "a18", ans3: "a19", ans4: "a20", correct: "a18", used: false } //A5
+    //     , { ans1: "a21", ans2: "a22", ans3: "a23", ans4: "a24", correct: "a21", used: false } //A6
+    //     , { ans1: "a25", ans2: "a26", ans3: "a27", ans4: "a28", correct: "a26", used: false }]; //A7
---
+++ assets/javascript/app2.js
@@ -0,0 +1,256 @@
+var game = {
+
+    timerElem: $("#timer"),
+    startBtn: $("#start"),
+    ans1: $("#answer1"),
+    ans2: $("#answer2"),
+    ans3: $("#answer3"),
+    ans4: $("#answer4"),
+    qSection: $("#question"),
+    maxTime: 5,
+    timeRem: 5,
+    timer1: null,
+    timer2: null,
+    timer2max: 3,
+    timer2left: 3,
+    //rand number generator
+    randNumGen: function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+    }, //works cuz copy pasta
+
+    questions: [
+        {
+            qid: 0,
+            q: "Q1: answer is 2",
+            potentialA: ["A1", "A2", "A3", "A4"],
+            a: "A2"
+        },
+        {
+            qid: 1,
+            q: "Q2: answer is 1",
+            potentialA: ["A1", "A2", "A3", "A4"],
+            a: "A1"
+        },
+        {
+            qid: 2,
+            q: "Q3 answer is 4",
+            potentialA: ["A1", "A2", "A3", "A4"],
+            a: "A4"
+        },
+        {
+            qid: 3,
+            q: "Q4 answer is 2",
+            potentialA: ["A1", "A2", "A3", "A4"],
+            a: "A2"
+        },
+        {
+            qid: 4,
+            q: "Q5 answer is 1",
+            potentialA: ["A1", "A2", "A3", "A4"],
+            a: "A1"
+        },
+        {
+            qid: 5,
+            q: "Q6 answer is 3",
+            potentialA: ["A1", "A2", "A3", "A4"],
+            a: "A3"
+        },
+        {
+            qid: 6,
+            q: "Q7 answer is 4",
+            potentialA: ["A1", "A2", "A3", "A4"],
+            a: "A4"
+        },
+        {
+            qid: 7,
+            q: "Q8 1",
+            potentialA: ["A1", "A2", "A3", "A4"],
+            a: "A1"
+        },
+        {
+            qid: 8,
+            q: "Q9 3",
+            potentialA: ["A1", "A2", "A3", "A4"],
+            a: "A3"
+        },
+    ], //end of questions
+
+
+    state: {
+        currentObj: {},
+        currentQId: 0,
+        currentQ: "",
+        potentialAnswers: [],
+        answer: "",
+        numCorrect: 0,
+        numWrong: 0,
+        questionsDone: [],
+        numQuestionsDone: 0,
+        chosenAns: "",
+    }, //end of state
+
+    chooseQ: function () {
+        var chosenQid = 0;
+        if (game.state.numQuestionsDone < 8) {
+            chosenQid = game.randNumGen(0, game.questions.length - 1)
+            if (game.state.questionsDone.includes(chosenQid)) {
+                game.chooseQ();
+            }
+            else {
+                var theQ = game.questions[chosenQid];
+                game.state.currentObj = theQ;
+                game.state.currentQ = theQ.q;
+                game.state.currentQId = theQ.qid;
+                game.state.potentialAnswers = theQ.potentialA;
+                game.state.answer = theQ.a;
+                game.state.questionsDone.push(game.state.currentQId);
+                game.display();
+                console.log(game.state.questionsDone);
+            }
+        }
+        else {
+            game.qSection.text("Game Over! You had " + game.state.numCorrect +
+                " Correct Answers, and " + game.state.numWrong + " Wrong Answers.");
+        }
+    },
+
+    compareAnswer: function () {
+        if (game.state.chosenAns === game.state.answer) {
+            game.state.numCorrect++;
+            game.qSection.text("YOU ARE CORRECT!!");
+        }
+        else {
+            game.state.numWrong++;
+            game.qSection.text("Too Bad!! Correct Answer is " + game.state.answer);
+        }
+    },
+
+    display: function () {
+        game.qSection.text(game.state.currentQ);
+        game.timerElem.text(game.timeRem);
+        game.ans1.text(game.state.potentialAnswers[0]);
+        game.ans2.text(game.state.potentialAnswers[1]);
+        game.ans3.text(game.state.potentialAnswers[2]);
+        game.ans4.text(game.state.potentialAnswers[3]);
+    },
+
+    initGame: function () {
+        game.chooseQ();
+    },
+
+    resetGame: function () {
+        game.state.currentObj = {};
+        game.state.currentQId = 0;
+        game.state.currentQ = "";
+        game.state.potentialAnswers = [];
+        game.state.answer = "";
+        game.state.numCorrect = 0;
+        game.state.numWrong = 0;
+        game.state.questionsDone = [];
+        game.state.numQuestionsDone = 0;
+        game.state.chosenAns = "";
+        game.maxTime = 5;
+        game.timeRem = 5;
+        game.timer2max = 5;
+        game.timer2left = 5;
+    },
+
+    timer1Run: function () {
+        game.timer1 = setInterval(game.decrement, 1000);
+    },
+
+    stopTimer1: function () {
+        clearInterval(game.timer1);
+    },
+
+    // timer2Run: function () {
+    //     game.timer2 = setInterval(game.decrement2, 1000);
+    // },
+
+    // stopTimer2: function () {
+    //     clearInterval(game.timer2);
+    // },
+
+    decrement: function () {
+        game.timeRem--;
+        game.timerElem.text(game.timeRem);
+        if (game.timeRem === 0) {
+            game.state.numWrong++;
+            game.qSection.text("Too Bad!! Correct Answer is: " + game.state.answer);
+            game.nextQTimer();
+            game.stopTimer1();
+        }
+    },
+
+    nextQTimer: function () {
+        game.timer2left = game.timer2max;
+        setInterval(function () {
+            game.timer2left--;
+            if (game.timer2left === 0) {
+                game.chooseQ();
+                clearInterval(game.timer2left);
+                game.timeRem = game.maxTime;
+                game.timerElem.text(game.timeRem);
+                game.timer1Run();
+            }
+        }, 1000);
+    },
+
+    // decrement2: function () {
+    //     game.timer2left--;
+    //     if (game.timer2left === 0) {
+    //         game.chooseQ();
+    //         game.stopTimer2();
+    //         game.timer2left = game.timer2max;
+    //     }
+    // },
+
+    play: function () {
+        $(document).ready(function () {
+            game.initGame();
+            console.log(game.state);
+        })
+    } //end of play
+
+}
+//====START BUTTON FUNCTION====
+game.startBtn.click(function () {
+    game.resetGame();
+    game.stopTimer1();
+    game.timer1Run();
+    game.play();
+});
+
+//====ANSWER BUTTON FUNCTIONS====
+game.ans1.click(function () {
+    game.state.chosenAns = game.ans1.text();
+    game.compareAnswer();
+    game.stopTimer1();
+    game.nextQTimer();
+    console.log(game.state.chosenAns);
+});
+game.ans2.click(function () {
+    game.state.chosenAns = game.ans2.text();
+    game.compareAnswer();
+    game.stopTimer1();
+    game.nextQTimer();
+    console.log(game.state.chosenAns);
+});
+game.ans3.click(function () {
+    game.state.chosenAns = game.ans3.text();
+    game.compareAnswer();
+    game.stopTimer1();
+    game.nextQTimer();
+    console.log(game.state.chosenAns);
+});
+game.ans4.click(function () {
+    game.state.chosenAns = game.ans4.text();
+    game.compareAnswer();
+    game.stopTimer1();
+    game.nextQTimer();
+    console.log(game.state.chosenAns);
+});
+
+
